809611bcc43bf91af7d7ac40c06543a8cdcbb7c8
thuthuin9601 <96036874+thuthuin9601@users.noreply.github.com>
2021-12-27 16:26:39 +0700

    done bai4.html

---
+++ bai4.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt("Enter the width");
+inputHeight=prompt("Enter the height");
+let a=parseInt(inputWidth);
+let b=parseInt(inputHeight);
+let c=a*b;
+document.write("the area is "+c);
